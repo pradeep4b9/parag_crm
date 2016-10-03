078c2960167c81c0c323362bd6bc81372aa2f096
pradeep <venkatakv72@gmail.com>
2016-10-03 16:45:40 +0530

    updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,41 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+set :application, 'parag_crm'
+set :repo_url, 'git@github.com:pradeep4b9/parag_crm.git'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :deploy_to, '/home/ubuntu/parag_crm'
+set :scm, :git
+# set :branch, 'master'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :keep_releases, 5
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :format, :pretty
+set :log_level, :debug
+set :pty,  false
+set :assets_roles, [:web, :app]
+set :rails_env, 'production'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
 
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, true
+set :puma_preload_app, true
+
+
 namespace :deploy do
+
+  # before :starting, :copy_puma
 
   desc 'Restart application'
   task :restart do
@@ -44,8 +45,6 @@ namespace :deploy do
     end
   end
 
-  after :publishing, :restart
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -54,5 +53,14 @@ namespace :deploy do
       # end
     end
   end
+
+  task :add_default_hooks do
+    after 'deploy:starting', 'sidekiq:quiet'
+    after 'deploy:updated', 'sidekiq:stop'
+    after 'deploy:reverted', 'sidekiq:stop'
+    after 'deploy:published', 'sidekiq:start'
+  end
+
+  after :finishing, 'deploy:cleanup'
 
 end
