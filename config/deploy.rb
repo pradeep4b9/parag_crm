5fd28d3e2c2df9e65b8c04ccafd6271b24de814a
pradeep <venkatakv72@gmail.com>
2016-10-05 14:44:42 +0530

    updayed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,64 +1,51 @@
-set :application, 'parag_crm'
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'web_portal'
 set :repo_url, 'git@github.com:pradeep4b9/parag_crm.git'
 
-set :deploy_to, '/home/ubuntu/parag_crm'
+set :branch, "production"
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/ubuntu/new_project'
+
+# Default value for :scm is :git
 set :scm, :git
-# set :branch, 'master'
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :keep_releases, 5
-
-set :format, :pretty
-set :log_level, :debug
-set :pty,  false
-set :assets_roles, [:web, :app]
-set :rails_env, 'production'
-
 set :user, "ubuntu"
 set :domain, "52.32.8.127"
+# set :scm_passphrase, "password"
+# set :use_sudo, false
+set :rails_env, "production"
+# set :deploy_via, :copy
+set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: ["publickey"],
+    keys: ["/home/pradeep/ec2/carmatec.pem"]
+    #auth_methods: %w(password)
+}
+role :ubuntu, "ubuntu@52.32.8.127"
 
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, true
-set :puma_preload_app, true
-
-
-namespace :deploy do
-
-  # before :starting, :copy_puma
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
- 
-
-  after :finishing, 'deploy:cleanup'
-
-end
+# Default value for keep_releases is 5
+set :keep_releases, 5
