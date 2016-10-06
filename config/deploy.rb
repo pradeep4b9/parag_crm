e563b92944ac7a1795ee255acb8461e1e21ccf64
pradeep <venkatakv72@gmail.com>
2016-10-06 12:56:01 +0530

    updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,19 +26,19 @@ set :ssh_options, {
 }
 role :ubuntu, "ubuntu@52.66.11.223"
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, true
-set :puma_preload_app, true
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_error.log"
+# set :puma_error_log, "#{shared_path}/log/puma_access.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 16]
+# set :puma_workers, 0
+# set :puma_init_active_record, true
+# set :puma_preload_app, true
 # Default value for :pty is false
 set :pty, true
 
@@ -47,30 +47,30 @@ set :pty, true
 set :keep_releases, 5
 
 
-namespace :deploy do
+# namespace :deploy do
 
-  # before :starting, :copy_puma
+#   # before :starting, :copy_puma
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       # Your restart mechanism here, for example:
+#       # execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
 
 
-  after :finishing, 'deploy:cleanup'
+#   after :finishing, 'deploy:cleanup'
 
-end
+# end
 
 
